09311b4693dbd64142cffdba7f251be19168869f
Vladislav Hadzhiyski <vladislav.hadzhiyski@gmail.com>
2019-11-30 20:31:28 +0200

    Add support to overwrite existing files after doing rename

--- src/FileWarden.Core/Rename/RenameWardenOptions.cs
+++ src/FileWarden.Core/Rename/RenameWardenOptions.cs
@@ -6,13 +6,14 @@ namespace FileWarden.Core.Rename
 {
     public class RenameWardenOptions : IWardenBaseOptions, IAppendSuffixWardenOptions
     {
-        public RenameWardenOptions(string source, SearchOption search, string suffix, bool createBackup, bool noCleanup)
+        public RenameWardenOptions(string source, SearchOption search, string suffix, bool createBackup, bool noCleanup, bool overwriteExistingFiles)
         {
             Source = source;
             Search = search;
             Suffix = suffix;
             CreateBackup = createBackup;
             NoCleanup = noCleanup;
+            OverwriteExistingFiles = overwriteExistingFiles;
         }
 
         public string Source { get; }
@@ -20,5 +21,6 @@ namespace FileWarden.Core.Rename
         public string Suffix { get; }
         public bool CreateBackup { get; }
         public bool NoCleanup { get; }
+        public bool OverwriteExistingFiles { get; set; }
     }
 }
