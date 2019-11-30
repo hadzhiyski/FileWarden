7fdbd296eb46dbf35edaa409550fc3c97c451b6e
Vladislav Hadzhiyski <vladislav.hadzhiyski@gmail.com>
2019-11-30 14:17:22 +0200

    Update README.md and add short options names

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # FileWarden
+Application for file system bulk operations.
+<br/>
+Supported operations
+- Rename
+
+# FileWarden.Cli
+
+## Rename
+
+`warden rename --source "C:\test" --suffix "_1" --recursive --backup`
+<br/>
+`warden rename --source "C:\test" --suffix "_1" -rb`
--- src/FileWarden.Cli/Options/RenameOptions.cs
+++ src/FileWarden.Cli/Options/RenameOptions.cs
@@ -14,16 +14,16 @@ namespace FileWarden.Cli.Options
         //[Option('r', "prefix", HelpText = "Prefix to append to file name", SetName = "prefix", Required = true)]
         //public string Prefix { get; set; }
 
-        [Option('s', "suffix", HelpText = "Suffix to append to file name", SetName = "suffix", Required = true)]
+        [Option("suffix", HelpText = "Suffix to append to file name", SetName = "suffix", Required = true)]
         public string Suffix { get; set; }
 
         //[Option("ignore", HelpText = "Ignore file name regex")]
         //public string IgnoreRegex { get; set; }
 
-        [Option("recursive", HelpText = "When 'true' it will rename files only in top level directory, otherwise it will rename all inner files. Default value is 'false'", Default = false)]
+        [Option('r', "recursive", HelpText = "When 'true' it will rename files only in top level directory, otherwise it will rename all inner files. Default value is 'false'", Default = false)]
         public bool Recursive { get; set; }
 
-        [Option('b', "backup", HelpText = "When 'true' it will create backup directory with the original directory. Default value is 'true'", Default = true)]
+        [Option('b', "backup", HelpText = "When 'true' it will create backup directory with the original directory. Default value is 'false'", Default = false)]
         public bool CreateBackup { get; set; }
     }
 }
