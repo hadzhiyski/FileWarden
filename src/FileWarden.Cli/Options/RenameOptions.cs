d396393f8bf46a53252e06b06998e9d1307c41b0
Vladislav Hadzhiyski <vladislav.hadzhiyski@gmail.com>
2019-12-01 21:41:22 +0200

    Make prefix and suffix options to work together

--- src/FileWarden.Cli/Options/RenameOptions.cs
+++ src/FileWarden.Cli/Options/RenameOptions.cs
@@ -14,10 +14,10 @@ namespace FileWarden.Cli.Options
         [Option("source", HelpText = "Source directory", Required = true)]
         public string Source { get; set; }
 
-        [Option("prefix", HelpText = "Prefix to append to file name", SetName = "prefix", Required = true)]
+        [Option("prefix", HelpText = "Prefix to append to file name")]
         public string Prefix { get; set; }
 
-        [Option("suffix", HelpText = "Suffix to append to file name", SetName = "suffix", Required = true)]
+        [Option("suffix", HelpText = "Suffix to append to file name")]
         public string Suffix { get; set; }
 
         [Option('r', "recursive", HelpText = "When 'true' it will rename files only in top level directory, otherwise it will rename all inner files", Default = false)]
